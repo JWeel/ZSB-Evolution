64ba7bd0396854d6901a56275bb8647fe367c529
Joseph <josefweel@gmail.com>
2014-06-26 18:19:50 +0200

    merge

--- src/framework/Main.java
+++ src/framework/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Screen screen = new Screen("Evolution V1.34");		
+		Screen screen = new Screen("Evolution V1.35 FINAL");		
 		screen.core.run();
 	}
 
